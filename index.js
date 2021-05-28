1291df36a2be85c433d049739a086f9741aa0817
jrc03c <jrc03c@pm.me>
2021-05-28 16:09:52 -0500

    ...

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const tools = require("js-math-tools")
+const { assert, isNumber, int, seed, random } = require("js-math-tools")
 
 function makeKey(keyLength, keySeed) {
-  tools.misc.assert(
-    tools.math.isNumber(keyLength) && parseInt(keyLength) === keyLength,
+  assert(
+    isNumber(keyLength) && int(keyLength) === keyLength,
     "`keyLength` must be an integer!"
   )
 
   if (keySeed) {
-    tools.misc.assert(
-      tools.math.isNumber(keySeed) && parseInt(keySeed) === keySeed,
+    assert(
+      isNumber(keySeed) && int(keySeed) === keySeed,
       "`keySeed` must be an integer!"
     )
 
-    tools.math.seed(keySeed)
+    seed(keySeed)
   }
 
   let out = ""
   let alpha = "abcdefghijklmnopqrstuvwxyz1234567890"
 
   for (let i = 0; i < keyLength; i++) {
-    out += alpha[parseInt(tools.math.random() * alpha.length)]
+    out += alpha[int(random() * alpha.length)]
   }
 
   return out
